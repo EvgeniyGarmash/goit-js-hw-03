3a3bd35494fccbce669183c368fcabbf352a22eb
EvgeniyGarmash <42199307+EvgeniyGarmash@users.noreply.github.com>
2023-12-23 18:55:39 +0200

    12

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,15 @@
-function getElementWidth(content, padding, border){
-    let resolt = Number.parseFloat(content) + (Number.parseFloat(padding) * 2) + (Number.parseFloat(border) * 2);
+function filterArray(numbers, value){
+    let resolt = [];
+    for (const item of numbers) {
+        if (item > value) {
+            resolt.push(item);
+        }
+    }
     return resolt;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
